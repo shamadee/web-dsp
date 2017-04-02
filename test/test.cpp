5cd80db1486ba0eae441007722cc26160907030f
shahrodkh <shahrodkh@gmail.com>
2017-04-01 17:56:36 -0700

    removed tests for funcs no longer in library

--- test/test.cpp
+++ test/test.cpp
@@ -3,19 +3,6 @@
 #include "./lib/catch.hpp"
 #include "../cpp/webdsp.cpp"
 
-// TEST_CASE("Testing simple doubler function", "[doubler]") {
-
-//   SECTION("Doubling simple positive and negative integers") {
-//     REQUIRE( doubler(1) == 2 );
-//     REQUIRE( doubler(100) == 200);
-//   }
-//   SECTION("Testing edge cases") {
-//     REQUIRE( doubler(0) == 0 );
-//     REQUIRE( doubler(-1) == -2);
-//     REQUIRE( doubler(12.75) == 24);
-//   }
-// }
-
 TEST_CASE("Testing grayScale function", "[grayScale]") {
   int len = 4;
   unsigned char data[] = { 12, 15, 199, 124 };
